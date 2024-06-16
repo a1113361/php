fbd52f4cfc08d779a212cb20170e2c04e3b1373e
a1113361 <125753947+a1113361@users.noreply.github.com>
2024-06-16 21:56:06 +0800

    Add files via upload

---
+++ week3/otherside.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>自我介紹</title>
+</head>
+<body bgcolor="#e6f3fb">
+    <center>
+        <h1>新網頁</h1>
+    </center>
+    <h2><font size="5" color="#87ceeb">早安!</font></h2>
+    <p>開開心心快快樂樂:D</p>
+</body>
+</html>
---
+++ week3/resume.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>自我介紹</title>
+</head>
+<body bgcolor="#e6f3fb">
+    <a name="#top"></a>
+    <center>
+        <h1>自我介紹</h1>
+    </center>
+    <h2><font size="5" color="#87ceeb">基本資料</font></h2>
+        <ul>
+            <li>姓名: 陳欣妘</li>
+            <li>學號: A1113361</li>
+            <li>性別: 女</li>
+            <li>興趣: 享受人生、體驗新事物</li>
+        </ul>
+    <h2><font size="5" color="#87ceeb">音樂九宮格</font></h2>
+        <table>
+            <tr>
+                <th>想到的順序</th>
+                <th>歌手/樂團</th>
+                <th>最愛的歌</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>兔巴兔</td>
+                <td>Deja Vu</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>草東沒有派對</td>
+                <td>床</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>fjc w/ lvoris & Ryce</td>
+                <td>Roundabout</td>
+            </tr>
+        </table>
+    </br>
+    <a href="#top">回最上面</a><br/>
+    <a href="otherside.php">新網頁</a>
+</body>
+</html>
